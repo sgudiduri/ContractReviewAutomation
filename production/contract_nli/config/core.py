9fcba1c8916f215b9500140a6072b3d4f522307d
sgudiduri <siddu.gemeni@gmail.com>
2023-02-02 21:00:11 -0600

    Checking in code adding pydantic

--- production/contract_nli/config/core.py
+++ production/contract_nli/config/core.py
@@ -1,19 +1,83 @@
+from pathlib import Path
+import typing as t
+from pydantic import BaseModel, validator
+from strictyaml import load, YAML
 import os
 
-class Core():
-    def __init__(self):        
-        self.PWD = os.path.dirname(os.path.abspath(__file__))
-        self.PACKAGE_ROOT = os.path.abspath(os.path.join(self.PWD, '..'))
-        self.TRAINED_MODEL_DIR = os.path.join(self.PACKAGE_ROOT, 'trained_model')
-        self.DATA_DIR = os.path.join(self.PACKAGE_ROOT, 'data')
 
-        #DATA PATH
-        self.TRAIN_PATH = "train.csv"
-        self.TEST_PATH = "test.csv"
+# Project Directories
+PWD = os.path.dirname(os.path.abspath(__file__))
+PACKAGE_ROOT = Path(os.path.abspath(os.path.join(PWD, '..')))
+ROOT = PACKAGE_ROOT.parent
+CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yaml"
+TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_model"
+DATA_DIR = PACKAGE_ROOT / "data"
 
-        # MODEL PERSISTING
-        self.VOCAB_PATH = "vocab.pth"
-        self.MODEL_PATH = "model.pth"
+class AppConfig(BaseModel):
+    """
+    Application-level config.
+    """
+    package_name: str
+    train_path: str
+    test_path: str
+    vocab_path: str
+    model_path: str
 
-        self.embed_size= 100
-        self.num_hiddens= 200  
+
+class ModelConfig(BaseModel):
+    """
+    All configuration relevant to model
+    training and feature engineering.
+    """
+    num_step: int
+    batch_size: int
+    learning_rate: float
+    embed_size: int
+    num_hiddens: int
+    epochs: int
+    save_best: bool
+    trainer: str
+    loss: str
+
+class Config(BaseModel):
+    """Master config object."""
+
+    app_config: AppConfig
+    model_config: ModelConfig
+
+
+def find_config_file() -> Path:
+    """Locate the configuration file."""
+    if CONFIG_FILE_PATH.is_file():
+        return CONFIG_FILE_PATH
+    raise Exception(f"Config not found at {CONFIG_FILE_PATH!r}")
+
+
+def fetch_config_from_yaml(cfg_path: Path = None) -> YAML:
+    """Parse YAML containing the package configuration."""
+
+    if not cfg_path:
+        cfg_path = find_config_file()
+
+    if cfg_path:
+        with open(cfg_path, "r") as conf_file:
+            parsed_config = load(conf_file.read())
+            return parsed_config
+    raise OSError(f"Did not find config file at path: {cfg_path}")
+
+
+def create_and_validate_config(parsed_config: YAML = None) -> Config:
+    """Run validation on config values."""
+    if parsed_config is None:
+        parsed_config = fetch_config_from_yaml()
+
+    # specify the data attribute from the strictyaml YAML type.
+    _config = Config(
+        app_config=AppConfig(**parsed_config.data),
+        model_config=ModelConfig(**parsed_config.data),
+    )
+
+    return _config
+
+
+config = create_and_validate_config()
--- production/contract_nli/main.py
+++ production/contract_nli/main.py
@@ -5,20 +5,17 @@ from d2l import torch as d2l
 from contract_nli.preprocessing.data_management import DataService
 from contract_nli.train import Train
 from contract_nli.predict import Predict
-from contract_nli.config.core import Core
+from contract_nli.config.core import config, TRAINED_MODEL_DIR, DATA_DIR
 
 # Open the file and load the file
-with open('config/config.yaml') as f:
-    config = yaml.load(f, Loader=SafeLoader)
-    print(config)
+trained_model_dir_path = TRAINED_MODEL_DIR.as_posix()
+data_dir_path = DATA_DIR.as_posix()
 
-c = Core()
-vocab_path = f"{c.TRAINED_MODEL_DIR}/{c.VOCAB_PATH}"
-model_path = f"{c.TRAINED_MODEL_DIR}/{c.MODEL_PATH}"
-train_path = f"{c.DATA_DIR}/{c.TRAIN_PATH}"
-test_path = f"{c.DATA_DIR}/{c.TEST_PATH}"
+vocab_path = f"{trained_model_dir_path}/{config.app_config.vocab_path}"
+model_path = f"{trained_model_dir_path}/{config.app_config.model_path}"
+train_path = f"{data_dir_path}/{config.app_config.train_path}"
+test_path = f"{data_dir_path}/{config.app_config.test_path}"
 
-args = config['Train']
 ds = DataService()
 train_set,test_set= ds.load_data(train_path, test_path)
 device = d2l.try_all_gpus()
@@ -27,13 +24,14 @@ train_iter,test_iter,vocab= ds.create_snli_dataset(train=train_set, \
                                                    test=test_set, num_workers=num_workers)
 
 #Train
+model_config = config.model_config
 tr = Train()
-tr.run_training(train_iter, test_iter, vocab,args["learning_rate"],\
-                args["epochs"],args["embed_size"],args["num_hiddens"],
+tr.run_training(train_iter, test_iter, vocab,model_config.learning_rate,\
+                model_config.epochs,model_config.embed_size,model_config.num_hiddens,
                 device, model_path, vocab_path) 
 
 #Predict
-pr = Predict(args["embed_size"],args["num_hiddens"], model_path, vocab_path)
+pr = Predict(model_config.embed_size,model_config.num_hiddens, model_path, vocab_path)
 pr.make_multiple_prediction_with_classification_report(test_set)
 
 
